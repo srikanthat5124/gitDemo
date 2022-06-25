c4cb28e67ac8090532b83bd598f598ee6e2374eb
srikanthat5121 <srikanthat5121@gmail.com>
2022-06-25 19:10:56 +0530

    second commit i can add a another new line

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,12 @@ public class Hello {
     public static  void main(String args[]){
         System.out.println("welcome Thanos");
         System.out.println("welcome Thanos");
+        System.out.println("welcome Thanos");
+        System.out.println("welcome Thanos");
+
+        System.out.println("welcome Thanos");
+        System.out.println("welcome Thanos");
+
 
     }
 
